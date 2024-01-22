8d971023a56da48f53b38a231697dc847d6cd6e0
grossiValentina <grossivalentina435@gmail.com>
2024-01-22 15:06:16 +0100

    add: gestione errori store

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -8,43 +8,54 @@
     <div class="row justify-content-center pt-3">
         <div class="col-6">
 
+            {{-- Validation - error message --}}
+            @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
             <form action="{{ route('comics.store') }}" method="POST">
                 @csrf
                 <div class="mb-3">
                     <label for="title" class="form-label">Titolo</label>
-                    <input type="text" class="form-control" id="title" name="title">
+                    <input type="text" class="form-control" id="title" name="title" value="{{ old('title') }}">
 
                     <div class="mb-3">
                         <label for="decription" class="form-label">Descrizione</label>
-                        <textarea class="form-control" id="description" name="description"></textarea>
+                        <textarea class="form-control" id="description" name="description">{{ old('description') }}</textarea>
                     </div>
 
                     <div class="mb-3">
                         <label for="thumb" class="form-label">Immagine</label>
-                        <input type="text" class="form-control" id="thumb" name="thumb">
+                        <input type="text" class="form-control" id="thumb" name="thumb" value="{{ old('thumb') }}">
                     </div>
 
                     <div class="mb-3">
                         <label for="price" class="form-label">Prezzo</label>
-                        <input type="text" class="form-control" id="price" name="price">
+                        <input type="text" class="form-control" id="price" name="price" value="{{ old('price') }}">
                     </div>
 
                     <div class="mb-3">
                         <label for="series" class="form-label">Serie</label>
-                        <input type="text" class="form-control" id="series" name="series">
+                        <input type="text" class="form-control" id="series" name="series" value="{{ old('series') }}">
                     </div>
 
                     <div class="mb-3">
                         <label for="sale_date" class="form-label">Data</label>
-                        <input type="text" class="form-control" id="sale_date" name="sale_date">
+                        <input type="text" class="form-control" id="sale_date" name="sale_date" value="{{ old('sale_date') }}">
                     </div>
 
                     <div class="mb-3">
                         <label for="type" class="form-label">Tipologia</label>
                         <select class="form-select" name="type" id="type">
                             <option value="">Seleziona</option>
-                            <option value="comic book">Comic Book</option>
-                            <option value="graphic novel">Graphic Novel</option>
+                            <option  @selected(old('type') === 'comic book')  value="comic book">Comic Book</option>
+                            <option  @selected(old('type') === 'graphic novel')  value="graphic novel">Graphic Novel</option>
                         </select>
                     </div>
 
